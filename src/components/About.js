8cd1784ea28c18866b73be349812bf6fb1edb80d
samyukrishnasamy <frv9sc@virginia.edu>
2025-08-18 23:51:43 -0400

    about me

--- src/components/About.js
+++ src/components/About.js
@@ -5,15 +5,17 @@ const About = () => {
     <section id="about">
       <h2>About Me</h2>
       <div className="about-content-wrapper">
+        <img src="assets/headshot.jpeg" alt="Samyu Krishnasamy" className="about-profile-pic" />
         <div className="about-main-text">
           <p>
             I am a rising senior at the University of Virginia studying Computer Science and Data Science.
             I love being adventures, whether it is exploring new places, trying new foods, or learning new skills.
           </p>
         </div>
-
-        <img src="assets/headshot.jpeg" alt="Samyu Krishnasamy" className="about-profile-pic" />
-
+        <div className="about-buttons">
+          <a href="samyukrishnasamy.github.io/assets/resume.pdf" target="_blank" rel="noopener noreferrer" className="nav-button">View Resume</a>
+          <a href="samyukrishnasamy.github.io/assets/transcript.pdf" target="_blank" rel="noopener noreferrer" className="nav-button">View Transcript</a>
+        </div>
       </div>
     </section>
   );
