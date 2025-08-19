a03209a1b6bac57450a846d547f3cef19e531ed6
samyukrishnasamy <frv9sc@virginia.edu>
2025-08-18 22:50:37 -0400

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,23 +4,20 @@ const Projects = () => {
   const projects = [
     {
       id: 1,
-      title: "Project 1",
-      description: "A web application built with React and Node.js",
-      technologies: ["React", "Node.js", "MongoDB"],
+      title: "Project",
+      description: "Coming Soon!",
       link: "#"
     },
     {
       id: 2,
-      title: "Project 2", 
-      description: "A mobile app developed using React Native",
-      technologies: ["React Native", "Firebase"],
+      title: "Project", 
+      description: "Coming Soon!",
       link: "#"
     },
     {
       id: 3,
-      title: "Project 3",
-      description: "A data analysis project using Python",
-      technologies: ["Python", "Pandas", "Matplotlib"],
+      title: "Project",
+      description: "Coming Soon!",
       link: "#"
     }
   ];
@@ -33,11 +30,6 @@ const Projects = () => {
           <div key={project.id} className="project-card">
             <h3>{project.title}</h3>
             <p>{project.description}</p>
-            <div className="technologies">
-              {project.technologies.map(tech => (
-                <span key={tech} className="tech-tag">{tech}</span>
-              ))}
-            </div>
             <a href={project.link} className="project-link">View Project</a>
           </div>
         ))}
